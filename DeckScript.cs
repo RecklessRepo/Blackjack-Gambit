5de0d7fbf5b2c886c8ee2f4631268ac8f5daea54
RecklessRepo <168262041+RecklessRepo@users.noreply.github.com>
2025-03-27 12:43:30 -0400

    Add files via upload

---
+++ DeckScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DeckScript : MonoBehaviour
+{
+    public Sprite[] cardSprites;
+    int[] cardValues = new int[53];
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void GetCardValues()
+    {
+        
+    }
+}
